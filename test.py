4109f6403287bb0bde79da4051393f9fb4897506
LeaveitBlank01 <[202280102@psu.palawan.edu.ph]>
2024-05-25 22:39:28 +0800

    added the test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+from api import app 
+
+class MyAppTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+    
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+
+    def test_get_people(self):
+        response = self.app.get("/people")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("John" in response.data.decode())  
+
+if __name__ == "__main__":
+    unittest.main()
